425234e802d1c35212edb87cb55d95b6c5ad75c9
sajaadasyntax <nozanteam@gmail.com>
2025-10-16 15:41:26 +0300

    nbnm

---
+++ ecosystem.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+    apps: [{
+      name: 'pp-backend',
+      script: 'src/app.js',
+      instances: 1,
+      exec_mode: 'cluster',
+      env: {
+        NODE_ENV: 'production',
+        PORT: 5000
+      },
+      error_file: './logs/err.log',
+      out_file: './logs/out.log',
+      log_file: './logs/combined.log',
+      time: true
+    }]
+  }
+  
