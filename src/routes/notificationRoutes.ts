05dd11c58039636c7ee67861cbf5125d83178ad5
sajaadasyntax <nozanteam@gmail.com>
2025-12-07 19:35:06 +0200

    committed

--- src/routes/notificationRoutes.ts
+++ src/routes/notificationRoutes.ts
@@ -1,5 +1,5 @@
 import { Router } from 'express';
-import { authenticate } from '../middleware/auth';
+import { authenticate } from '../middlewares/auth';
 import * as notificationController from '../controllers/notificationController';
 
 const router = Router();
